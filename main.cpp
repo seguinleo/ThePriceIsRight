2da950d293bed2f06b00115847b5903a576ac91c
PouletEnSlip <97188999+PouletEnSlip@users.noreply.github.com>
2022-04-09 16:35:21 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*
+Nom : JusteNombre
+Rôle : Choisit un nombre au hasard entre 1 et 1000 et demande à l’utilisateur de le retrouver
+Auteur : Léo Seguin
+Date : 12/10/2020
+*/
+
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+
+{
+    srand(time(NULL));
+    short nbGagnant, nb;
+    nbGagnant = rand() %1000 + 1;
+    cout<<"Salut BG !"<<endl;
+    while (nb!=nbGagnant)
+    {
+            cout<<"Saisissez un nombre entre 1 et 1000."<<endl;
+            cin>>nb;
+
+            if (nb<nbGagnant)
+            {
+                cout<<"Le nombre gagnant est plus grand ! Recomencez."<<endl;
+            }
+            if (nb>nbGagnant)
+            {
+                    cout<<"Le nombre gagnant est plus petit. Recomencez."<<endl;
+            }
+
+            if (nb==nbGagnant)
+            {
+                    cout<<"Felicitation ! Vous avez trouve le nombre gagnant ! Vous n'avez rien gagne par contre ;)"<<endl;
+            }
+            system("PAUSE");
+    }
+    return 0;
+}
